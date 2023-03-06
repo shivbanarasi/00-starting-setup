b4f8705c49d99c6b02f443b24eab7eabcda7d40d
shivbanarasi <shivbanarasi0542@gmail.com>
2023-03-06 19:47:12 +0530

    Design the User and Product relationship

--- app.js
+++ app.js
@@ -12,6 +12,8 @@ app.set('views', 'views');
 
 const adminData = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
+const product=require('./models/product');
+const user=require('./models/user');
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
@@ -20,5 +22,17 @@ app.use('/admin', adminData);
 app.use(shopRoutes);
 
 app.use(err404.get404);
+
+product.belongsTo(user,{constraits:true,onDelete:'CASCADE'});
+user.hasMany(product);
+
+sequelize
+.sync({force: true})
+.then(result=>{
+    app.listen(3000);
+})
+.catch(err=>{
+    console.log(err);  
+})
 
 app.listen(3000);
